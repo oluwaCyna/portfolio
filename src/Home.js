1fabc3abc47e5b5c6b09a4b24229a69abc62464c
oluwaCyna <oluwashinaayomi2019@gmail.com>
2022-06-25 23:12:46 -0700

    Update button links

--- src/Home.js
+++ src/Home.js
@@ -6,28 +6,41 @@ import AboutCard from "./AboutCard";
 import "./Home.css";
 
 const Home = () => {
+  let passiveClassName = "nav-link px-md-5 px-sm-3";
+  let activeClassName = "nav-link px-md-5 px-sm-3 active";
+
   return (
     <div className="container container-sm bg-dark bordered mt-0 mt-sm-5 pb-3">
       <div className="row">
         <div className="col-sm-6 width position height">
           <h2 className="text-white part-1">My name is</h2>
           <div className="content part-3">
-            <h1 className=""><pre>Shina A.</pre></h1>
-            <h1 className=""><pre>Shina A.</pre></h1>
+            <h1 className="">
+              <pre>Shina A.</pre>
+            </h1>
+            <h1 className="">
+              <pre>Shina A.</pre>
+            </h1>
           </div>
-          <h2 className="part-4 animate-charcter">I am a FULL STACK DEVELOPER</h2>
+          <h2 className="part-4 animate-charcter">
+            I am a FULL STACK DEVELOPER
+          </h2>
           <div className="part-5">
-            <div className="d-flex gap-4">
-            <a href="./CV" className="btn btn-primary px-4">
-                CV
-              </a>
-              <a href="./portfolio" className="btn btn-primary">
-                Portfolio
-              </a>
+            <div className="gap-4 nav nav-pills d-flex justify-content-center py-3">
+              <Link to="CV">
+                <button className="btn btn-primary px-4">CV</button>
+              </Link>
+              <Link to="Portfolio">
+                <button className="btn btn-primary">Portfolio</button>
+              </Link>
             </div>
             <div className="d-flex gap-3 py-4 text-white">
-            <a href="https://www.linkedin.com/in/shinaa"><FontAwesomeIcon icon={faLinkedinIn} /></a>
-            <a href="https://github.com/oluwaCyna"><FontAwesomeIcon icon={faGithub} /></a>
+              <a href="https://www.linkedin.com/in/shinaa">
+                <FontAwesomeIcon icon={faLinkedinIn} />
+              </a>
+              <a href="https://github.com/oluwaCyna">
+                <FontAwesomeIcon icon={faGithub} />
+              </a>
             </div>
           </div>
         </div>
@@ -35,8 +48,6 @@ const Home = () => {
           <AboutCard />
         </div>
       </div>
-
-      
     </div>
   );
 };
